43f156c5e8a755611fca36162f3a2d42b2daa0de
BerryYoghurt <jehadaly@gmail.com>
2020-02-20 13:16:01 +0200

    reformatted the directories according to what is required

--- assignment1/Classes/calculator/Calculator.java
+++ assignment1/Classes/calculator/Calculator.java
@@ -1,22 +1,5 @@
 package calculator;
 
-interface ICalculator {
-/**
-* Adds given two numbers
-* @param x first number
-* @param y second number
-* @return the sum of the two numbers
-*/
-int add(int x, int y);
-/**
-* Divides two numbers
-* @param x first number
-* @param y second number
-* @return the division result
-*/
-float divide(int x, int y) throws RuntimeException;
-}
-
 public class Calculator implements ICalculator {
 	
 	public int add(int x, int y) {
