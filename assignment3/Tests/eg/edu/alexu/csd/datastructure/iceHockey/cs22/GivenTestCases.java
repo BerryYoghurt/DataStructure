f746f03f86a96e40a9010a47ba5ea58ecc445d7c
BerryYoghurt <jehadaly@gmail.com>
2020-03-05 21:19:43 +0200

    added assignment 3
    Signed-off-by: BerryYoghurt <jehadaly@gmail.com>

--- assignment3/Tests/eg/edu/alexu/csd/datastructure/iceHockey/cs22/GivenTestCases.java
+++ assignment3/Tests/eg/edu/alexu/csd/datastructure/iceHockey/cs22/GivenTestCases.java
@@ -81,4 +81,19 @@ public class GivenTestCases {
 		expected = new Point[] {new Point(5,5), new Point(5,5)};
 		assertArrayEquals(expected,points);
 	}
+	@Test
+	public void test7() {
+		photo = new String[] {"","",""};
+		points = f.findPlayers(photo, 4, 16);
+		expected =new Point[] {};
+		assertArrayEquals(expected,points);
+	}
+	
+	@Test
+	public void test8() {
+		photo = new String[] {"44444H44S4","K444K4L444","4LJ44T44XH","444O4VIF44","44C4D4U444","4V4Y4KB4M4","G4W4HP4O4W","4444ZDQ4S4","4BR4Y4A444","4G4V4T4444"};
+		points = f.findPlayers(photo, 5, 16);
+		expected =new Point[] {};
+		assertArrayEquals(expected,points);
+	}
 }
