75c9684c231d6caa83ed9fcafc5ac6382ac0fda3
BerryYoghurt <jehadaly@gmail.com>
2020-04-02 14:53:07 +0200

    assignment5

--- assignment5/Classes/eg/edu/alexu/csd/datastructure/stack/Stack.java
+++ assignment5/Classes/eg/edu/alexu/csd/datastructure/stack/Stack.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package eg.edu.alexu.csd.datastructure.stack;
 
 import java.util.NoSuchElementException;
@@ -64,70 +63,3 @@ public class Stack implements IStack{
 	}
 
 }
-=======
-package eg.edu.alexu.csd.datastructure.stack;
-
-import java.util.NoSuchElementException;
-
-
-public class Stack implements IStack{
-
-	private class Node{
-		Object data;
-		Node next;
-		Node(Object data, Node next){
-			this.data = data;
-			this.next = next;
-		}
-	}
-	
-	private int size;
-	private Node top; 
-	
-	public Stack(){
-		this.size = 0;
-		this.top = null;
-	}
-	
-	@Override
-	public Object pop() {
-		if(size != 0) {
-			Object topData = top.data;
-			top = top.next;
-			size--;
-			return topData;
-		}
-		else {
-			throw new NoSuchElementException("Stack empty");
-		}
-	}
-
-	@Override
-	public Object peek() {
-		if(size != 0) {
-			return top.data;
-		}
-		else {
-			throw new NoSuchElementException("Stack Empty");
-		}
-	}
-
-	@Override
-	public void push(Object element) {
-		Node newNode = new Node(element, this.top);
-		this.top = newNode;
-		size++;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return this.top == null;
-	}
-
-	@Override
-	public int size() {
-		return this.size;
-	}
-
-}
->>>>>>> d7a4750f329a2af912a30500c137e49590a0efd2
